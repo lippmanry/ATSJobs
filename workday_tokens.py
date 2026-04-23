6a9391e13d38ec36be9386c43079ff3dc686f274
Ryan J Lippman <ryan@datacat-security.com>
2026-04-23 15:53:13 +0100

    switched to ddg for workday

--- workday_tokens.py
+++ workday_tokens.py
@@ -1,13 +1,9 @@
-import requests
+
 import re
 import random
 import time
-from urllib.parse import quote_plus
+from ddgs import DDGS
 from datetime import datetime
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 from utils import save_tokens_mongo
@@ -24,89 +20,32 @@ token_collection = db['workday_tokens']
 
 
 token_collection = db['workday_tokens']
-#github version
-def get_driver():
-    options = uc.ChromeOptions()
-    options.add_argument('--headless')  
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
-    driver = uc.Chrome(options=options, use_subprocess=True)
-    
-    return driver
-
-def workday_token_search(keyword, limit=10, start=0, gl="ca"):
-    keywords = " ".join([f'"{w}"' for w in keyword.split()])
-    raw_query = f'site:myworkdayjobs.com {keywords}'
-    query = quote_plus(raw_query)
-    
+def workday_token_search(site, keyword):
     tokens = set()
+    query = f'site:{site} {keyword}'
+    print(f"Searching DDG for {query}")
     
-    driver = get_driver()
-    print(f"Searching: https://www.google.com/search?q={query}&num={limit}&gl={gl}&start={start}")
-
+    system_blacklist = {'embed', 'search', 'v1', 'd', 'api', 'js', 'widgets', 'careers'}
+    pattern = r"([a-zA-Z0-9_-]+)\.(wd[0-9]+)\.myworkdayjobs\.com"
     
-    try:
-        driver.get("https://www.google.com")
-        time.sleep(random.uniform(2, 4))
-        
-        search_url = f"https://www.google.com/search?q={query}&num={limit}&gl={gl}&start={start}"
-        # ddg_search_url = f"https://duckduckgo.com/html/?q=site:myworkdayjobs.com+{query}"
-        driver.get(search_url)
-        if "Forbidden" in driver.title or "403" in driver.title:
-            print(f"Still getting 403. Wait.")
-            return []
-        
-        #deal with potential consent banner
-        try:
-
-            wait = WebDriverWait(driver, 5) 
-            consent_btn = wait.until(EC.element_to_be_clickable((By.ID, 'W0wltc')))
-            consent_btn.click()
-            print("Consent clicked.")
-            time.sleep(2)                
-        except:
-            #continue as normal if no consent banner
-            print("No consent required.")
-            pass            
-
-        
-        time.sleep(random.uniform(7,12))
-        try:
-            WebDriverWait(driver, 10).until(EC.title_contains("Search"))
-        except:
-            pass
-        #emulate human scrolling to try and avoid scraper flag
-        driver.execute_script(f"window.scrollBy(0, {random.randint(400,900)});")
-
-        print(f"Page Title: {driver.title}")
-        #find link elements
-        links = driver.find_elements(By.XPATH, "//a[contains(@href, 'myworkdayjobs.com')]")
-        
-        for link in links:
-            url = link.get_attribute("href")
-            if url:
-                match = re.search(r"([a-zA-Z0-9_-]+)\.(wd[0-9]+)\.myworkdayjobs\.com", url)
-                if match:
-                    tenant = match.group(1).lower()
-                    datacenter = match.group(2).lower()
+    with DDGS() as ddgs:
+        results = ddgs.text(query, max_results=20)
+        for r in results:
+            url = r['href']
+            match = re.search(pattern, url)
+            if match:
+                tenant = match.group(1).lower()
+                datacenter = match.group(2).lower()
+                if tenant not in system_blacklist:
                     tokens.add(f"{tenant}:{datacenter}")
-
-        
-    except Exception as e:
-        print(f"An error has occurred: {e}")     
-    finally:
-        driver.quit()
-        
     return list(tokens)
 def workday_new_tokens():
     #search for multiple industries via common operations
-    anchor_words = ["Analyst", "Developer", "Manager", "Operations", "IT", "Engineer", "Compliance", "Staff", "Information Technology", "Analytics", "Security"]
+    anchor_words = ["Analyst", "Developer", "Manager", "Operations", "IT", "Engineer", "Compliance", "Staff", "Analytics", "Security"]
     locations = ["Canada", "United Kingdom", "UK", "Global", "Remote", "EMEA", "North America"]
-    gls = ["us", "ca", "uk"]
+    site = "myworkdayjobs.com"
     successful_runs = 0
+    new_tokens = set()
     
     while successful_runs < 3:
         new_found = {}
@@ -114,12 +53,12 @@ def workday_new_tokens():
         word = random.choice(anchor_words)
         location = random.choice(locations)
         query = f'{word} {location}'
-        start_index = random.choice([0,10])
-        gl = random.choice(gls)
-        new_found = workday_token_search(keyword=query, limit=20, start=start_index, gl=gl)
+        
+        new_found = workday_token_search(site,keyword=query)
         
         if new_found:
             save_tokens_mongo(new_found, token_collection=token_collection)
+            new_tokens.update(new_found)
             successful_runs += 1
             print(f"{len(new_found)} new tokens found on [{datetime.now()}]: {new_found}") 
             
@@ -129,9 +68,12 @@ def workday_new_tokens():
                 time.sleep(wait_time)
         
         else:
-            wait_time = random.randint(30,60)
-            print(f"No new tokens found for {query}. Trying new keywords in {wait_time}s...")
-            time.sleep(wait_time)
+            print(f"No new tokens found found for {query}. Retrying...")
+        wait_time = random.uniform(15,30)
+        print(f"Sleeping for {round(wait_time, 2)}s...")
+        time.sleep(wait_time)
+    print(f"Total unique tokens found: {len(new_tokens)}")
+    return list(new_tokens)
 
 if __name__ == "__main__":
     workday_new_tokens()
